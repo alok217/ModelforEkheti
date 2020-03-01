ca9866589b5a946c34c750add090487d9815d3aa
Alok Pandey <alokrocks217@gmail.com>
2020-03-01 13:12:53 +0530

    Adjust buildpacks on Heroku

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import sys
 import json
 import requests
 import numpy as np
+import os
 
 from flask import Flask, request
 from model import prediction
@@ -29,3 +30,6 @@ def home():
     
 if __name__ == "__main__":
     app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
