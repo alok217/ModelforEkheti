d7b4e160ffb7df47ef821d4fcd9c62abe356c0e8
Alok Pandey <alokrocks217@gmail.com>
2020-03-01 13:25:27 +0530

    Adjust buildpacks on Heroku

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def home():
     # return "Hi"
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host= '0.0.0.0',debug=True)
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
