c8344307a776d433ca61a379e55d19683eebfc74
Alok Pandey <alokrocks217@gmail.com>
2020-03-02 11:12:08 +0530

    Adjust buildpacks on Heroku

---
+++ server.py
@@ -0,0 +1,35 @@
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+import keras
+import sys
+import json
+import requests
+import numpy as np
+import os
+
+from flask import Flask, request
+from model import prediction
+app = Flask(__name__)
+
+@app.route("/",methods = ['POST'])
+def home():
+    # print(request.form)
+    data=request.form.get('data')
+    print(data)
+    h = prediction(str(data))
+    # print('hi',h)
+    return str(h)
+    # return "Hi"
+
+def main():
+    app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    main()
--- wsgi.py
+++ wsgi.py
@@ -1,34 +1,6 @@
-import pandas
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
-from keras.utils import np_utils
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
-import keras
-import sys
-import json
-import requests
-import numpy as np
-import os
+from server import app
 
-from flask import Flask, request
-from model import prediction
-app = Flask(__name__)
-
-@app.route("/",methods = ['POST'])
-def home():
-    # print(request.form)
-    data=request.form.get('data')
-    print(data)
-    h = prediction(str(data))
-    # print('hi',h)
-    return str(h)
-    # return "Hi"
-    
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0',debug=True)
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    app.run()
+    
+
